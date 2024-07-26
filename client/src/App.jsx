1f5bb356ec636b4d2d470362330bc9d5dbd1d9d1
YogeshJat8965 <yogeshjat8965@gmail.com>
2024-07-27 03:49:46 +0530

    gauge chart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
 import { Component } from "./components/Linegraph";
-// import { Linegraph } from "./components/Linegraph";
-import { Button } from "./components/ui/button";
+import GaugeChart from "react-gauge-chart";
+// import { Percent } from "api"; fetch api and use the value of water utilised today
 
 function App() {
   return (
     <>
-      <Button>Fuck me </Button>
       <Component></Component>
+      <div className="gauge-container"  >
+        <GaugeChart id="gauge-chart1" 
+          textColor="black"
+          // percent={percent} get percent through the api
+          ></GaugeChart>
+          <div className='font-black text-2xl ' >Water Utilisation Today</div>
+
+      </div>
+        
     </>
   );
 }

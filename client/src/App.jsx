568d72628a541790342346f25d23d2f257e8af8a
yugal1107 <yugal1107@gmail.com>
2024-07-27 01:46:53 +0530

    Solved tailwind issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Component } from "./components/Linegraph";
+import Navbar from "./components/Navbar/Navbar";
 // import { Linegraph } from "./components/Linegraph";
 import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <Button>Fuck me </Button>
+      <Navbar />
       <Component></Component>
     </>
   );

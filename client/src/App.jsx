522bd795545cba8503e98aff2260d54a65ded967
yugal1107 <yugal1107@gmail.com>
2024-07-28 04:12:39 +0530

    Added cluster page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,19 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutUs from "./components/Navbar/AboutUs";
+import ClusterPage from "./pages/ClusterPage";
 
 function App() {
   return (
     <>
-     <Router>
-     <Navbar />
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-      </Routes>
-     
-     
-     </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/cluster" element={<ClusterPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }

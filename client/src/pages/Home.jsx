faf3c73d6aa54d80641b0f357b582ca9bf125dac
yugal1107 <yugal1107@gmail.com>
2024-07-27 09:25:52 +0530

    Some UI changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,25 @@
 import Component from "@/components/BarChart";
 import React, { useEffect } from "react";
-
+import GaugeChart from "react-gauge-chart";
 
 export default function Home() {
   return (
-    <div className="p-5 grid grid-cols-1 md:grid-cols-3">
-      <Component> </Component>
-      <div></div>
+    <div className="p-5 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="shadow-md border">
+        <Component> </Component>
+      </div>
+      <div className="hidden md:block"></div>
+      <div className="flex flex-col gap-4 justify-center items-center border rounded-lg shadow-md">
+        <GaugeChart
+          id="gauge-chart1"
+          textColor="black"
+          // percent={percent} get percent through the api
+        ></GaugeChart>
+        <div className="font-black text-2xl ">Water Utilisation Today</div>
+      </div>
     </div>
   );
+}
+{
+  /* <div className="gauge-container"  ></div> */
 }

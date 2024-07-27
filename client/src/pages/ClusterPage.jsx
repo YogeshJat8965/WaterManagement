5d4e4dffb9467df2f47b6b719322fe1d58c22922
yugal1107 <yugal1107@gmail.com>
2024-07-27 23:32:17 +0530

    Changes

---
+++ client/src/pages/ClusterPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ClusterPage() {
+  <div className="m-3 p-2 border rounded-lg">
+    <div className="border-2">
+      <iframe
+        src="https://yugal1107-classifier-model.hf.space"
+        frameborder="0"
+        width="850"
+        height="450"
+      ></iframe>
+    </div>
+  </div>;
+}

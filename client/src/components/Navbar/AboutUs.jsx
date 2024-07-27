51db6b082ab8a62d50654c58495e80f0dd72f2c1
Tejaswa Jhode <jhodetejswa2004@gmail.com>
2024-07-27 23:23:43 +0530

    About page added

---
+++ client/src/components/Navbar/AboutUs.jsx
@@ -0,0 +1,141 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className=" absolute top-16 w-full bg-gray-100 p-8">
+      <h1 className="text-5xl font-extrabold mb-8 text-center animate-fade-in">About Us</h1>
+
+      
+
+      <div className="mb-8 p-6 bg-white shadow-lg rounded-lg animate-slide-up">
+        <p className="text-xl mb-4">
+          This page is designed to help you learn more about our company and what we do. We are a team of passionate individuals who are committed to providing our customers with the best possible products and services.
+        </p>
+      </div>
+{/* 
+      <div className="col-span-1 md:col-span-2 flex justify-center mb-8 gap-20 mt-5">
+        <div >
+        <img src="https://media.istockphoto.com/id/1468125168/photo/rajwada-palace.jpg?b=1&s=612x612&w=0&k=20&c=ePO3DAImuzemE2fWe_9BCDFhO8d-B5MV9qYoMBays18=" alt="Rajwada Palace" className="w-96 h-96 rounded-lg shadow-lg animate-fade-in" />
+        
+
+        </div>
+        <div >
+        <img src="https://media.istockphoto.com/id/539001564/photo/rajwada-palace-indore.webp?s=2048x2048&w=is&k=20&c=fu6-uMqvp_0X8XPqjZaHIdJZFsJMf3Po7Hm8Ln3WheI=" alt="Rajwada Palace" className="w-96 h-96 rounded-lg shadow-lg animate-fade-in" />
+        
+
+        </div>
+
+        <div >
+          <img src="https://media.istockphoto.com/id/515708494/photo/mahatma-gandhi-hall.jpg?s=2048x2048&w=is&k=20&c=3QsjUazEtQ8gX3fkB7ZIGjBeYRY-3GRsK9UMg86AM4I=" alt="Sarafa Bazar" className="w-96 h-96 rounded-lg shadow-lg animate-fade-in" />
+        </div>
+        </div> */}
+
+
+      <div className="flex justify-center mb-8">
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="p-6 bg-white shadow-lg rounded-lg animate-slide-up">
+          <h2 className="text-3xl font-bold mb-4">Our Mission</h2>
+          <p className="text-lg">
+            Our mission is to provide our customers with the best possible products and services. We are committed to providing excellent customer service and to exceeding our customers' expectations.
+          </p>
+        </div>
+        
+        <div className="p-6 bg-white shadow-lg rounded-lg animate-slide-up">
+          <h2 className="text-3xl font-bold mb-4">Our Vision</h2>
+          <p className="text-lg">
+            Our vision is to be the leading provider of products and services in our industry. We are committed to innovation and to providing our customers with the latest and most advanced products and services.
+          </p>
+        </div>
+
+        
+        
+        
+        <div className="p-6 bg-white shadow-lg rounded-lg animate-slide-up">
+          <h2 className="text-3xl font-bold mb-4">Our Values</h2>
+          <ul className="list-disc list-inside text-lg">
+            <li>Customer Focus: We are committed to providing our customers with the best possible products and services.</li>
+            <li>Innovation: We are committed to innovation and to providing our customers with the latest and most advanced products and services.</li>
+            <li>Excellence: We are committed to excellence in everything we do.</li>
+            <li>Integrity: We are committed to integrity and to doing the right thing.</li>
+            <li>Teamwork: We are committed to teamwork and to working together to achieve our goals.</li>
+          </ul>
+        </div>
+        
+        <div className="p-6 bg-white shadow-lg rounded-lg animate-slide-up">
+          <h2 className="text-3xl font-bold mb-4">Our Team</h2>
+          <p className="text-lg">
+            Our team is made up of passionate and talented individuals who are committed to providing our customers with the best possible products and services. We have a diverse range of skills and experience, and we are always learning and growing.
+          </p>
+        </div>
+        
+        {/* <div className="col-span-1 md:col-span-2 flex justify-center mb-8 gap-20">
+        <div >
+          <img src="https://media.istockphoto.com/id/515708494/photo/mahatma-gandhi-hall.jpg?s=2048x2048&w=is&k=20&c=3QsjUazEtQ8gX3fkB7ZIGjBeYRY-3GRsK9UMg86AM4I=" alt="Sarafa Bazar" className="w-96 h-96 rounded-lg shadow-lg animate-fade-in" />
+        </div>
+
+        <div>
+        <img src="https://media.istockphoto.com/id/515708494/photo/mahatma-gandhi-hall.jpg?s=2048x2048&w=is&k=20&c=3QsjUazEtQ8gX3fkB7ZIGjBeYRY-3GRsK9UMg86AM4I=" alt="Sarafa Bazar" className="w-96 h-96 rounded-lg shadow-lg animate-fade-in" />
+
+        </div>
+        </div> */}
+        
+        <div className="p-6 bg-white shadow-lg rounded-lg animate-slide-up">
+          <h2 className="text-3xl font-bold mb-4">Our Customers</h2>
+          <p className="text-lg">
+            Our customers are the heart of our business. We are committed to providing our customers with the best possible products and services, and to exceeding their expectations.
+          </p>
+        </div>
+        
+        <div className="p-6 bg-white shadow-lg rounded-lg animate-slide-up">
+          <h2 className="text-3xl font-bold mb-4">Our Community</h2>
+          <p className="text-lg">
+            We are committed to being a good corporate citizen and to giving back to our community. We support a number of charities and organizations, and we are always looking for ways to make a difference.
+          </p>
+        </div>
+      </div>
+
+     
+      <div className="col-span-1 md:col-span-2 flex justify-center mb-8 gap-20 mt-5">
+        <div >
+        <img src="https://media.istockphoto.com/id/1468125168/photo/rajwada-palace.jpg?b=1&s=612x612&w=0&k=20&c=ePO3DAImuzemE2fWe_9BCDFhO8d-B5MV9qYoMBays18=" alt="Rajwada Palace" className="w-96 h-96 rounded-lg shadow-lg animate-fade-in" />
+        
+
+        </div>
+        <div >
+        <img src="https://media.istockphoto.com/id/539001564/photo/rajwada-palace-indore.webp?s=2048x2048&w=is&k=20&c=fu6-uMqvp_0X8XPqjZaHIdJZFsJMf3Po7Hm8Ln3WheI=" alt="Rajwada Palace" className="w-96 h-96 rounded-lg shadow-lg animate-fade-in" />
+        
+
+        </div>
+
+        <div >
+          <img src="https://media.istockphoto.com/id/515708494/photo/mahatma-gandhi-hall.jpg?s=2048x2048&w=is&k=20&c=3QsjUazEtQ8gX3fkB7ZIGjBeYRY-3GRsK9UMg86AM4I=" alt="Sarafa Bazar" className="w-96 h-96 rounded-lg shadow-lg animate-fade-in" />
+        </div>
+
+        <div>
+        <img src="https://media.istockphoto.com/id/177636133/photo/gate-of-mandu-indore.jpg?s=2048x2048&w=is&k=20&c=bm8oKqglqy4BLfqhCZoBQzWa-HiU_JF-Ym93TQZNehE=" alt="Lal Bagh Palace" className="w-auto h-96  " />
+      </div>
+        </div>
+
+
+      <div className="mt-8 p-6 bg-white shadow-lg rounded-lg animate-slide-up">
+        <h2 className="text-3xl font-bold mb-4">Contact Us</h2>
+        <p className="text-lg mb-2">If you have any questions or comments, please contact us at:</p>
+        <ul className="text-lg">
+          <li>Email: <a href="mailto:nn.indore@mpurban.gov.in" className="text-blue-500">nn.indore@mpurban.gov.in</a></li>
+          <li>Phone: <a href="tel:0731-2522111" className="text-blue-500">0731-2522111</a></li>
+          <li>Address: Jail Road, Nagar Nigam, Netaji Subash Marg, Nagar Nigam, Indore, Madhya Pradesh 452007</li>
+        </ul>
+      </div>
+
+
+     
+      <p className="text-center text-lg mt-8 animate-fade-in">
+        Thank you for visiting our website! We hope you found this information helpful.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import { Button } from "../ui/button";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate()
+
+ const handleAboutUs = () => {
+    navigate("/about");
+  }
+
+  const handleHome = () => {
+    navigate("/");
+  }
+
+  
+
   return (
     <nav className="border-b bg-sky-500 py-2">
       <div className="navbar  flex justify-between items-center">
         <div className="font-extrabold text-4xl ml-3">
           <h2>Logo</h2>
         </div>
-        <div className="flex gap-5 ml-5 items-center font-bold ">
-          <div>Home</div>
-          <div>About Me</div>
+        <div className="flex gap-5 ml-5 items-center font-bold cursor-pointer ">
+          <div onClick={handleHome}>Home</div>
+          <div onClick={handleAboutUs}>About Me</div>
           <div>Contact</div>
         </div>
         <Button className="mx-2">Sign In</Button>
